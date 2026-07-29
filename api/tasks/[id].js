0767fa64df6ecfffc186bd6ec0002901236ad9db
aadityajoshiwork-art <aadityajoshi.work@gmail.com>
2026-07-29 15:23:42 +0530

    Update [id].js

--- api/tasks/[id].js
+++ api/tasks/[id].js
@@ -5,6 +5,10 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 if (req.method === 'OPTIONS') return res.status(200).end();
   const { id } = req.query;
 
